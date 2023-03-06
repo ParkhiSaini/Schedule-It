ecc6bc6b7ee58ad71a16fc2e79028d75d442ae01
Khushi <khushitomar2001@gmail.com>
2023-03-06 15:10:49 +0530

    landing page updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import LandingPage from './components/LandingPage'
+import LandingPage from './components/LandingPage';
+import LoginPage from './components/LoginPage';
+
+
 function App() {
   return (
-    <div className="App">
-      
-    <LandingPage/>
-    </div>
+    <Router>
+      {/* <div className="App"> */}
+
+        <Routes>
+          
+          <Route path='/LandingPage' element={<LandingPage />} />
+          <Route path='/Login' element={<LoginPage />} />
+          
+        </Routes>
+      {/* </div> */}
+    </Router>
+    
+    
+    
+  
   );
 }
 
---
+++ client/src/components/LoginPage.js
@@ -0,0 +1,16 @@
+import React  from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import '../App.css';
+import { useNavigate } from 'react-router-dom';
+
+function LoginPage(){
+    return(
+        <div>
+            <h1>hello</h1>
+        </div>
+    );
+
+}
+
+export default LoginPage;
