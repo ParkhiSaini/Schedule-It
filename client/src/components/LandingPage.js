63f72615cc95b4200bcdc6e4564cae3e79fbf386
ParkhiSaini <parkhi.saini2401@gmail.com>
2023-03-06 19:59:14 +0530

    landing page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -6,34 +6,29 @@ import '../App.css';
 
 import { useNavigate } from 'react-router-dom';
 function LandingPage(){
-    return(<div class = "LandingPage">
+    return(
+
+    <div class = "LandingPage">
         <nav class="navbar navbar-light bg-light">
-            <a class="navbar-brand" href="#" >
-                Home 
-            </a>
-            <a class="navbar-brand2" href="#" >
-                About Us
-            </a>
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Home</a>
+                <a class="LogIn" href ="#">Login</a>
+                <a class="SignIn" href ="#">SignIn</a>
+            </div>
         </nav>
-        
-        <p class="scheduleit">SCHEDULE IT</p> 
-        
+        <p class="Scheduleit">SCHEDULE IT</p> 
         <div class="text1">
-        
-       
-        <p class="line1"><span class="brown">Scheduleit</span> a place where you can book meeting </p>
-        <p class="line2"><span class="brown"> room</span> and rooms for upcoming</p>
-        <p class="line3"><span class="brown">events</span>, avoiding the many hours of </p>
-        <p class="line4"><span class="brown">waiting</span> for approvals.</p>
-        <p class="line5"><span class="brown">Your quickest</span> access to ongoing </p> 
-        <p class= "line6"><span class="brown">events</span> and timetable </p>
-        
+        <p class="line1">Need to <span class="brown">Organise</span> an event? </p>
+        <p class="line2">Need a<span class="brown"> Discussion </span>room for your team?</p>
+        <p class="line3">Want the whole college to know about your <span class="brown">Event</span> </p>
+        <p class="line4">Want to know the latest<span class="brown"> happenings </span>in the college</p>
+        <p class="line5">Want to acess your<span class="brown"> time-table </span>in just a click</p> 
         </div>
        
         
         {/*button  type="button " class ="SignIn">Sign In</button>*/}
-        <button type="button" class="btn btn-outline-secondary">Sign In</button>  
-        <button class ="Login">Login </button>
+        <button type="button" class="btn">Book Now</button>
+        
         
 
 
