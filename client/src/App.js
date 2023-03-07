add91b31afdfc5b9eeda220bdc69b3147d180c47
ParkhiSaini <parkhi.saini2401@gmail.com>
2023-03-07 16:08:08 +0530

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import LandingPage from './components/LandingPage'
+import LandingPage from './components/LandingPage';
+
+import Requests from './components/Requests';
+
+
 function App() {
   return (
-    <div className="App">
-      
-    <LandingPage/>
-    </div>
+    <Router>
+      {/* <div className="App"> */}
+
+        <Routes>
+          
+          <Route path='/LandingPage' element={<LandingPage />} />
+         
+          <Route path='/Requests' element={<Requests />} />
+          
+        </Routes>
+      {/* </div> */}
+    </Router>
+    
+    
+    
+  
   );
 }
 
