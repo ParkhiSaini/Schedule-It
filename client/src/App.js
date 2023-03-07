77c355de55523da65c8d801174bd3c8235450b9e
Khushi <khushitomar2001@gmail.com>
2023-03-07 09:41:11 +0530

    log in

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LandingPage from './components/LandingPage';
 import LoginPage from './components/LoginPage';
-
+import SignupPage from './components/SignupPage';
+import BookRoom from './components/BookRoom';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
           
           <Route path='/LandingPage' element={<LandingPage />} />
           <Route path='/Login' element={<LoginPage />} />
+          <Route path='/Signup' element={<SignupPage />} />
+          <Route path='/BookRoom' element={<BookRoom />} />
           
         </Routes>
       {/* </div> */}
---
+++ client/src/components/BookRoom.js
@@ -0,0 +1,68 @@
+import React  from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import '../App.css';
+
+
+import { useNavigate } from 'react-router-dom';
+
+function BookRoom(){
+    return(<div class = "BookRoom">
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#" >
+                Home 
+            </a>
+            <a class="navbar-brand3" href="#" >
+                Username
+            </a>
+            <a class="navbar-brand4" href="#" >
+                Time-Table
+            </a>
+            <a class="navbar-brand5" href="#" >
+                Your Requests
+            </a>
+        </nav>
+        <p class="text3">Book Your Room</p>
+        <form class= "form3">
+            <div class="form_col1">
+                <div class="inputBox3">
+                    <input type="name" name="name" value="" />
+                    <label>Purpose</label>
+                </div>
+    <div class="inputBox3">
+        <input type="name" name="name" value="" />
+        <label>No.Of People</label>
+    </div>
+    <div class="inputBox3">
+        <input type="name" name="name" value="" />
+            <label>Club/Chapter</label>
+      <div class="inputBox3">
+                    <input type="name" name="name" value="" />
+                    <label>Room Type</label>
+                </div>                  
+    </div>
+    </div>
+    <div class="form_col2">
+                <div class="inputBox3">
+                    <input type="name" name="name" value="" />
+                    <label>Date-</label>
+                </div>
+    <div class="inputBox3">
+        <input type="name" name="name" value="" />
+        <label>Priority</label>
+    </div>
+    <div class="inputBox3">
+        <input type="name" name="name" value="" />
+            <label>Duration-</label>
+                        
+    </div>
+    </div>
+    
+    </form> 
+    <button class ="Book">Raise Ticket</button>
+        </div>
+
+        );
+      
+    }
+export default BookRoom;
