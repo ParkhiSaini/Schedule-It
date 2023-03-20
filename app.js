b42eab5b599a2f956450473649a4080d99dce543
Akash Sinha <akashsinha432003@gmail.com>
2023-03-20 19:44:28 +0530

    polished code

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-require('dotenv').config();
 
 const connectDB = require('./config/db');
 const routes = require('./routes/api/rooms');
 
-const authRoutes = require('./routes/auth');
-const { db } = require('./models/User');
 
 const app = express();
 connectDB();
 
-
-app.use(bodyParser.json());
-app.use(cors());
-
-app.use('/api', authRoutes);
+app.use('/api', routes);
 
 const port = process.env.PORT || 5000;
 
